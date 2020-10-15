5f70ac69c75a57d7c55676cbaca8bea948c5b46d
Jennifer Kramer <jenkrame@gmail.com>
2020-10-15 16:42:51 -0400

    Got the form working, fortunately!

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <div className="App">
+      {/* App links to Form component and sends 'people' and 'setPeople' as props */}
       <Form people={people} setPeople={setPeople}/>
+      {/* Make separate component for each person like 'MemberList'. Displaying data sent through props*/}
     </div>
   );
 }
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
+// grabbing info from schemaForm as 'yup'
 import * as yup from 'yup'
+
+// uses axios to post data
 import axios from 'axios'
 
+// form function that passes 'props'
 const Form = (props) => {
 
+    // useState grabs data through 'info', 'setInfo' is a function to change 'info'
     const [ info, setInfo ] = useState({
         fName: '',
         email: '',
@@ -11,35 +16,52 @@ const Form = (props) => {
         TOS: false
     })
 
+    // grabs the post info from the link
     const postInfo = (person) => {
         axios.post(`https://reqres.in/api/users`, person)
         .then((res) => {
-            props.setPeople(res)
+            console.log(res.data)
+            props.setPeople([...props.people, res.data])
         })
         .catch((err) => {
             console.log(err)
         })
     }
 
+    // gives the click an event to fire // toggle
     const onChange = (event) => {
         if (event.target.type === 'checkbox') {
             setInfo({
                 ...info,
                 TOS: !info.TOS
             })
+        } else {
+            setInfo({
+                ...info,
+                // one way
+                // fName:
+                // another way
+                [event.target.name]: event.target.value
+            })
         }
+    }
 
-    setInfo({...info,
-        [event.target.name]: event.target.value
-    })};
-
-
+    // gives the submit button a click function
     const onSubmit = (event) => {
+        //
         event.preventDefault()
-        props.setPeople([...info])
+        // refers to postInfo function and posts to the backend
         postInfo(info)
+        //setInfo becomes reset
+        setInfo({
+            fName: '',
+            email: '',
+            password: '',
+            TOS: false
+        })
     }    
 
+    // creates a div of information
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -47,27 +69,35 @@ const Form = (props) => {
                     <input 
                         id='name'
                         type='text'
-                        value={info.name}
-                        name='name'
+                        value={info.fName}
+                        name='fName'
                         placeholder='Name'
                         onChange={onChange}
-                    />Email: 
+                    />
+                </label>
+                <label>Email: 
                     <input
                         id='email'
                         type='text'
+                        name='email'
                         value={info.email}
                         email='email'
                         placeholder='Email'
                         onChange={onChange}
-                    />Password: 
+                        />
+                </label>
+                <label>Password: 
                     <input
                         id='password'
-                        type='text'
+                        type='password'
+                        name='password'
                         value={info.password}
                         password='password'
                         placeholder='Password'
                         onChange={onChange}
-                    />Terms of Service: 
+                    />
+                </label>
+                <label>Terms of Service: 
                     <input
                         id='TOS'
                         type='checkbox'
